6584e5c9f7ae381c4a552db5429142b3f2411863
FabioBara <fabio.bara@icloud.com>
2017-08-21 01:31:48 -0300

    init commint

---
+++ introducao-javascript/js/imc.js
@@ -0,0 +1,26 @@
+function imcFun(i){
+    var peso = pacientes[i].querySelector(".info-peso").textContent;
+    var altura = pacientes[i].querySelector(".info-altura").textContent;
+    var verificaAtributos = true;
+    
+    if(peso < 0 || peso > 200){
+        
+        pacientes[i].classList.add("paciente-invalido");
+        pacientes[i].querySelector(".info-imc").textContent = "Peso invalido!";
+        verificaAtributos = false;
+
+    }
+    
+    if(altura< 0 || altura > 2.5){
+        
+        pacientes[i].classList.add("paciente-invalido");
+        pacientes[i].querySelector(".info-imc").textContent = "Altura invalida!";
+        verificaAtributos = false;
+
+    }
+    if(verificaAtributos){
+        var imc = peso / (altura * altura);
+        pacientes[i].querySelector(".info-imc").textContent = imc.toFixed(2);
+    }
+    
+}
---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,54 @@
+
+var titulo = document.querySelector(".titulo");
+console.log(titulo.textContent);
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+
+for(var i = 0; i < pacientes.length; i++){
+    imcFun(i);
+}
+
+var botao = document.querySelector("#adicionar-paciente");
+
+botao.addEventListener("click", mostraMensagem);
+
+function mostraMensagem(){
+    event.preventDefault();
+    var form = document.querySelector("#formAdiciona");
+
+    var nome = form.nome.value;
+    var altura = form.altura.value;
+    var peso = form.peso.value;
+    var gordura = form.gordura.value;   
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    alturaTd.textContent = altura;
+    pesoTd.textContent = peso;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+}
+ 
+
+
+
+
+
